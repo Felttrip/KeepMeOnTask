e3a12bfc2ce10813bede072011e4f3d2bef99498
karrde00 <karrde00@gmail.com>
2015-02-28 14:37:30 -0600

    fixed adding and rendering

--- options.js
+++ options.js
@@ -1,47 +1,54 @@
 //page gets loaded
 $('document').ready(function(){
-	getList("whitelist");
-	getList("blacklist");
-	$('.remove').click(function(event){
+	renderList("whitelist");
+	renderList("blacklist");
+	$('.remove').click(function(){
 		siteId = $(event.currentTarget.parentElement).data("id");
-		siteType = $(event.currentTarget.parentElement).data("group");
-		removeSite(siteType, siteId);
+		listType = $(event.currentTarget.parentElement).data("group");
 		$(event.currentTarget.parentElement).remove();
-
-
+		removeSite(listType, siteId);
 	});
 	$('#whitelist_add').click(function(){
-		//chrome.storage.sync.clear();
 		var site = $('#whitelist_input')[0].value;
+		$('#whitelist_input').val('');
 		addToList("whitelist", site);
-		$('#whitelist_list').append("<li data-id='' data-group='whitelist'>"+site+" <span class='remove glyphicon glyphicon-remove'></span></li>");
+	});
+	$('#blacklist_add').click(function(){
+		var site = $('#blacklist_input')[0].value;
+		$('#blacklist_input').val('');
+		addToList("blacklist", site);
 	})
 
 })
 
-function getList(listType){
+function renderList(listType){
+	$("#"+listType+"_list").html('');
 	chrome.storage.sync.get(listType, function(items){
-		for (var i = 0; i < items[listType].length; i++) {
-			$('#'+listType+'_list').append("<li data-id=\""+i+"\" data-group=\""+listType+"\">"+items[listType][i]+"<span class='remove glyphicon glyphicon-remove'></span></li>");
+		if (items[listType]) {
+			for (var i = 0; i < items[listType].length; i++) {
+				$('#'+listType+'_list').append("<li data-id=\""+i+"\" data-group=\""+listType+"\">"+items[listType][i]+" <span class='remove glyphicon glyphicon-remove'></span></li>");
+			};
 		};
+		
 	})
 }
 
 function addToList(listName,siteName) {
 	chrome.storage.sync.get(listName, function(items){
 		var sites = [];
-		if(items[listName].constructor === Array){
+		if(items[listName] && items[listName].constructor === Array){
 			sites = items[listName];
 		}
 		sites.push(siteName);
 	 	var obj = {};
 	 	obj[listName] = sites;
 		chrome.storage.sync.set(obj, function() {
+		  renderList(listName);
 		  console.log('Settings saved');
 		});
 	});
 }
 
 function removeSite(listType, siteId){
-
+	
 }
