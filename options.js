01a1f644eabcc158c06be4a40bea980722338b65
Nate Thompson <nthompson55@gmail.com>
2015-02-28 10:34:38 -0600

    saving in progress

---
+++ options.js
@@ -0,0 +1,21 @@
+//page gets loaded
+document.addEventListener('DOMContentLoaded', function() {
+    var button = document.getElementById('add');
+    // onClick's logic below:
+    link.addEventListener('click', function() {
+		saveList(["test","tester","testeroni"],"blackListUrl");
+    });
+});
+
+
+function saveList(jsonArray,listType) {
+	// Check that there's some code there.
+	if (!jsonArray) {
+	  message('Error: No value specified');
+	  return;
+	}
+	chrome.storage.sync.set({listType: jsonArray}, function() {
+	  // Notify that we saved.
+	  message('Settings saved');
+	});
+}
